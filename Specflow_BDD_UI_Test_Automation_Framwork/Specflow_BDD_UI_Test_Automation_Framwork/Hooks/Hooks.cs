8053871035db6a13bcb0c78fb6f8bca855203e96
Salman Arshad <salmanarshad@humanfocus.co.uk>
2020-08-04 18:47:50 +0500

    Add Extent Report

--- Specflow_BDD_UI_Test_Automation_Framwork/Specflow_BDD_UI_Test_Automation_Framwork/Hooks/Hooks.cs
+++ Specflow_BDD_UI_Test_Automation_Framwork/Specflow_BDD_UI_Test_Automation_Framwork/Hooks/Hooks.cs
@@ -18,27 +18,102 @@ namespace Specflow_BDD_UI_Test_Automation_Framwork.Hooks
     public sealed class Hooks : Pages
     {
         private readonly ScenarioContext _scenarioContext;
-        public Hooks(ScenarioContext scenarioContext):base(scenarioContext)
+        private static ExtentTest featureName;
+        private static ExtentTest scenario;
+        private static AventStack.ExtentReports.ExtentReports extent;
+        public static ExtentTest test;
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+        private readonly IObjectContainer _objectContainer;
+        public Hooks(ScenarioContext scenarioContext, IObjectContainer objectContainer, FeatureContext featureContext) :base(scenarioContext)
         {
             _scenarioContext = scenarioContext;
+            _objectContainer = objectContainer;
         }
         //------------------------------------------------------------------
 
-
         [BeforeFeature]
-        public static void BeforeFeature(FeatureContext featureContext)
+        public static void BeforeFeature()
         {
-           
+            //Create dynamic feature name
+            featureName = extent.CreateTest<Feature>(FeatureContext.Current.FeatureInfo.Title);
+
         }
 
         [BeforeScenario]
         public void BeforeScenario()
         {
+            scenario = featureName.CreateNode<Scenario>(ScenarioContext.Current.ScenarioInfo.Title);
             var config = ConfigurationManager.Configuration();
             var isRemote = config["IsRemoteDriver"];
             DriverFactory factory = new DriverFactory(_scenarioContext);
             factory.Init(Browsers.Chrome, bool.Parse(isRemote));
             InitPages();
+        }
+        [BeforeTestRun]
+        public static void InitializeReport()
+        {
+            //Initialize Extent report before test starts
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+            outPutDirectory = outPutDirectory.Substring(0, outPutDirectory.IndexOf("bin"));
+
+            outPutDirectory = outPutDirectory.Substring(outPutDirectory.IndexOf("\\") + 1);
+            String path = Path.Combine(outPutDirectory, "TestResults\\index.html");
+            var htmlReporter = new ExtentHtmlReporter(path);
+            htmlReporter.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            //Attach report to reporter
+            extent = new AventStack.ExtentReports.ExtentReports();
+            extent.AttachReporter(htmlReporter);
+        }
+        [AfterStep]
+        public void InsertReportingSteps()
+        {
+
+
+            var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+            PropertyInfo pInfo = typeof(ScenarioContext).GetProperty("ScenarioExecutionStatus", BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo getter = pInfo.GetGetMethod(nonPublic: true);
+            object TestResult = getter.Invoke(ScenarioContext.Current, null);
+            //Pass status
+            if (ScenarioContext.Current.TestError == null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text);
+            }
+            //Failure Staus
+            if (ScenarioContext.Current.TestError != null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.Message);
+            }
+
+            //Pending Status
+            if (TestResult.ToString() == "StepDefinitionPending")
+            {
+                if (stepType == "Given")
+                    ScenarioContext.Current.Pending();
+                else if (stepType == "When")
+                    ScenarioContext.Current.Pending();
+                else if (stepType == "Then")
+                    ScenarioContext.Current.Pending();
+                else if (stepType == "And")
+                    ScenarioContext.Current.Pending();
+
+            }
+        }
+        [AfterTestRun]
+        public static void TearDownReport()
+        {
+            extent.Flush();
         }
         [AfterScenario]
         public void AfterScenario()
